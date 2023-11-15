81ac4eeae1075a3a828b719f98575fa63ef8b8ec
Gavin Taylor <gavintaylor.ai@gmail.com>
2023-11-14 19:43:58 -0600

    Changed jpg names. Added in functionality to switch between pictures using state and the buttons.

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Image from './image.jsx'
 import './App.css'
 
 export default function App() {
+
   return (
     <>
       <div id="title-container">
--- src/Image.jsx
+++ src/Image.jsx
@@ -1,58 +1,60 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faArrowLeft } from '@fortawesome/free-solid-svg-icons'
 import { faArrowRight } from '@fortawesome/free-solid-svg-icons'
-// import { useState } from 'react'
+import { useState } from 'react'
 import './Image.css'
 
-const leftArrow = <FontAwesomeIcon
-    id="left"
-    icon={faArrowLeft}
-    style={{color: "#ffffff"}}
-/>
-const rightArrow = <FontAwesomeIcon
-    id="right"
-    icon={faArrowRight}
-    style={{color: "#ffffff"}}
-/>
-
-// const [showImg, setShowImg] = useState([]);
+const leftArrow = <FontAwesomeIcon id="left" icon={faArrowLeft} style={{color: "#ffffff"}} />
+const rightArrow = <FontAwesomeIcon id="right" icon={faArrowRight} style={{color: "#ffffff"}} />
 
 export default function Image() {
-    // function populateShowImg() {
-    //     console.log('I made it');
-    //     for (let i = 0; i < 4; ++i) {
-    //         let addImg = {
-    //             id: i,
-    //             url: './assets/picture' + i + '.jpg'
-    //         }
-    //         setShowImg([...showImg, addImg]);
-    //     }
-    // }
+    const [showImg, setShowImg] = useState(0);
+
+    let imageObjectArray = [
+        { id: 0, url: './src/assets/picture0.jpg' },
+        { id: 1, url: './src/assets/picture1.jpg' }, 
+        { id: 2, url: './src/assets/picture2.jpg' }, 
+        { id: 3, url: './src/assets/picture3.jpg' }
+    ];
+
+    function changeImage(num, imageObjectArray) {
+        if (num === '0') {
+            if (showImg > 0) {setShowImg(showImg - 1);}
+        }
+        else {
+            if (showImg < imageObjectArray.length) {setShowImg(showImg + 1);}
+        }
+    }
         
-    function renderImages() {
-        // if id === x
-        //     foundImg === showImg.x
-        //     return (showImg.url)
+    function renderImages(imageObjectArray) {
+        let imgId = imageObjectArray[showImg];
+
         return (
             <>
-                <img className="img" src="./src/assets/picture1.jpg" alt="Purple lofi picture" />
+                <img className="img" src={imgId.url} alt="Purple lofi picture" />
             </>
         )
     }
 
     return (
         <>
-            {console.log('step 1')}
-            {/* {populateShowImg()} */}
             <div className="image-container">
                 <div className='carousel-container'>
-                    <button className='switchImgBtn'>{leftArrow}</button>
-                        {renderImages()}
-                        
-                    <button className='switchImgBtn'>{rightArrow}</button>
+                    <button
+                        className='switchImgBtn'
+                        id='left-btn'
+                        onClick={()=>{changeImage('0', imageObjectArray)}}>
+                        {leftArrow}
+                    </button>
+                        {renderImages(imageObjectArray)}                       
+                    <button 
+                        className='switchImgBtn' 
+                        id='right-btn' 
+                        onClick={()=>{changeImage('1', imageObjectArray)}}>
+                        {rightArrow}
+                    </button>
                 </div>
             </div>
         </>
     )
 }
-
